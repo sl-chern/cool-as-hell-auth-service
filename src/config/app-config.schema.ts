32c168018b092b24666a76ca93faaba7ae93ae96
sl-chern <slavikchernogor@gmail.com>
2025-07-29 17:52:42 +0300

    initial commit

---
+++ src/config/app-config.schema.ts
@@ -0,0 +1,9 @@
+import { nodeEnv } from "src/utils/constants";
+import { z } from "zod";
+
+export const environmentVariablesSchema = z.object({
+  NODE_ENV: z.enum([nodeEnv.test, nodeEnv.development, nodeEnv.production]),
+  PORT: z.string(),
+  GLOBAL_PREFIX: z.string(),
+  BODY_LIMIT: z.string(),
+});
---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "src/app.module";
+import { ZodFilter } from "src/filters/zod.filter";
+import { AllConfigType } from "./config/config.type";
+import { VersioningType } from "@nestjs/common";
+import * as bodyParser from "body-parser";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new ZodFilter());
+
+  const configService = app.get(ConfigService<AllConfigType>);
+
+  app.setGlobalPrefix(
+    configService.getOrThrow("app.globalPrefix", { infer: true }),
+  );
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: "1",
+  });
+
+  app.use(
+    bodyParser.json({
+      limit: configService.getOrThrow("app.bodyLimit", { infer: true }),
+    }),
+  );
+  app.use(
+    bodyParser.urlencoded({
+      limit: configService.getOrThrow("app.bodyLimit", { infer: true }),
+      extended: true,
+    }),
+  );
+
+  await app.listen(configService.getOrThrow("app.port", { infer: true }));
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
